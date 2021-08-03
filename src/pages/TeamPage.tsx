983b301e90483409c89bd4d67011c646f16eea4d
Brandon Davis <neusfear@gmail.com>
2021-08-03 15:15:15 -0500

    Add flip animation to employee card

--- src/pages/TeamPage.tsx
+++ src/pages/TeamPage.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import ReactCardFlip from 'react-card-flip'
 import brandon_serious from '../images/staff/brandon_serious.jpg'
 import brandon_silly from '../images/staff/brandon_silly.jpg'
 
@@ -16,29 +17,28 @@ const TeamCard = ({person}, {person: any}) => {
   const [hover, setHover] = useState(false);
   const [selected, setSlected] = useState(false);
 
-  if (selected) {
-    return(
-      <div className="lg:w-1/3 md:w-1/2 w-full h-1/2 text-center" onClick={() => setSlected(!selected)}>
-        <p className="text-6xl font-bold mt-8">{person.nickname.toUpperCase()}</p>
-        <p>{person.name}</p>
-        <p className="px-14 py-4 text-justify">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis.</p>
-      </div>
-    )
-  } else {
-    return(
-      <div className="lg:w-1/3 md:w-1/2 w-full h-1/2 bg-cover filter grayscale hover:grayscale-0" 
-        style={{backgroundImage: `url(${hover ? person.silly : person.serious})`}} 
-        onMouseLeave={() => setHover(false)} 
-        onMouseOver={() => setHover(true)}
-        onClick={() => setSlected(!selected)}>
-      </div>
-    )
-  }
+  return(
+    <div className="lg:w-1/3 md:w-1/2 w-full h-half">
+      <ReactCardFlip isFlipped={selected}>
+        <div className="bg-cover filter group grayscale hover:grayscale-0 w-full h-half" 
+          style={{backgroundImage: `url(${hover ? person.silly : person.serious})`, backgroundPosition: 'center'}}
+          onMouseLeave={() => setHover(false)}
+          onMouseOver={() => setHover(true)}
+          onClick={() => setSlected(!selected)}>
+        </div>
+        <div className="text-center w-full h-half" onClick={() => setSlected(!selected)}>
+          <p className="text-6xl font-bold mt-8">{person.nickname.toUpperCase()}</p>
+          <p>{person.name}</p>
+          <p className="px-14 py-4 text-justify">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.</p>
+        </div>
+      </ReactCardFlip>
+    </div>
+  )
 }
 
 const TeamCards = () => {
   return(
-    <div className="flex-wrap flex self-center w-full h-screen">
+    <div className="flex-wrap flex self-center w-screen min-h-screen">
       {teamMembers.map((d, dIdx) => <TeamCard key={dIdx} person={d} />)}
     </div>
   )
@@ -47,7 +47,7 @@ const TeamCards = () => {
 const TeamPage = () => {
     return (
       <>
-        <br/><br/><br/>
+        <br/><br/>
         <TeamCards />
       </>
     );
