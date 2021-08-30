375ddcacbc7c182555ef3fe749e6da9416eb4ac6
Brandon Davis <neusfear@gmail.com>
2021-08-30 11:32:37 -0500

    Add placeholder text to Press Page

--- src/pages/PressPage.tsx
+++ src/pages/PressPage.tsx
@@ -1,8 +1,16 @@
+import Footer from "../components/Footer";
+
 const PressPage = () => {
     return (
-      <>
-        <p>Press Page</p>
-      </>
+      <div>
+        <div className="flex flex-col h-screen">
+          <div className="flex-grow"></div>
+          <p className="w-screen text-center underline">Press Page Under Construction</p>
+          <p className="w-screen text-center">Check Back soon!</p>
+          <div className="flex-grow"></div>
+        </div>
+        <Footer />
+      </div>
     );
   };
   
