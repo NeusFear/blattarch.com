4c01bdeed3d9fe6d2c163a99f57a8b3623731a2f
blattjas <89794038+blattjas@users.noreply.github.com>
2021-11-15 11:35:53 -0600

    Update TeamPage.tsx

--- src/pages/TeamPage.tsx
+++ src/pages/TeamPage.tsx
@@ -17,7 +17,7 @@ const teamMembers = [
   { name: "Carlie Gruel", nickname: "Carlie", email: "cgruel@blattarch.com", title: "Project Architect", image: carlie},
   { name: "Veronica Secculini", nickname: "Veronica", email: "vseculini@blattarch.com", title: "Intern Architect", image: veronica },
   { name: "Yara Albotros", nickname: "Yara", email: "yalbotros@blattarch.com", title: "Intern Architect", image: yara },
-  { name: "Rainey High", nickname: "Rainey", email: "rhigh@blattarch.com", title: "Intern Architect", image: rainey },
+  { name: "Rainey High", nickname: "Rainey", email: "rhigh@blattarch.com", title: "Architect/Specification Writer", image: rainey },
   { name: "Billie Baker", nickname: "Billie", email: "billie@blattarch.com", title: "Financial Officer", image: billie},
   { name: "Brandon Davis", nickname: "Brandon", email: "bdavis@blattarch.com", title: "Student Intern Architect", image: brandon},
   { name: "Chloe Sweet", nickname: "Chloe", email: "csweet@blattarch.com", title: "Receptionist / Student Intern", image: undefined}
