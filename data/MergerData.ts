9c0ca67f39c4d7da26a33838ca2b3528721e864a
blattjas <89794038+blattjas@users.noreply.github.com>
2022-04-14 11:36:06 -0500

    Update MergerData.ts
    Tony Quote

--- data/MergerData.ts
+++ data/MergerData.ts
@@ -2,7 +2,7 @@ export const MergerRoute = "merger-announcement"
 
 export const MergerTitle = "Blatt Architects Merges with ADG"
 
-export const MergerBannerText = "Blatt Architects and ADG are proud to announce a merger of firms. Blatt Architects and ADG have collaborated on many progects in the OKC area and both have over 20 years of firm history. Read more about the merger in our press release using the link below."
+export const MergerBannerText = " "After working with the ADG team for 19 years, I have observed and appreciated a shared common vision, goals, and approach in our practices.  We are humbled for this opportunity to fashion an innovative firm with a focus on forward-thinking design, integrity, and service, and are confident that this merger will position the combined entity for growth to meet the expanding A/E and program management needs of the entire Central Oklahoma region." - Tony Blatt, President Blatt Architects"
 
 export const MergerPressLines = [
     "Some press release line",
